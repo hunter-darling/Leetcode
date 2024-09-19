83ba926cd1b833e1c1fe0ef8d83c0dac531009c6
Hunter Darling <hunterperryd@gmail.com>
2024-09-18 22:40:33 -0500

    longest substring with no repeating characters

---
+++ src/main/java/solutions/LongestSubstringWithNoRepeats.java
@@ -0,0 +1,35 @@
+package src.main.java.solutions;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithNoRepeats {
+  // First attempt: WRONG
+  // public int lengthOfLongestSubstring(String s) {
+  // HashSet<Character> charSet = new HashSet();
+  // int start = 0;
+  // int result = 0;
+  // for (int end = 1; end < s.length(); ++end) {
+  // while (charSet.contains(s.charAt(end))) {
+  // charSet.remove(s.charAt(start));
+  // start += 1;
+  // }
+  // charSet.add(s.charAt(end));
+  // result = Math.max(result, charSet.size());
+  // }
+  // return result;
+  // }
+  public int lengthOfLongestSubstring(String s) {
+    HashSet<Character> charSet = new HashSet<Character>();
+    int start = 0;
+    int result = 0;
+    for (int end = 0; end < s.length(); ++end) {
+      while (charSet.contains(s.charAt(end))) {
+        charSet.remove(s.charAt(start));
+        start += 1;
+      }
+      charSet.add(s.charAt(end));
+      result = Math.max(result, charSet.size());
+    }
+    return result;
+  }
+}
